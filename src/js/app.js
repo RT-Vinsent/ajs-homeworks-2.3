72a07a26400da774281a1c35c4dcc1e3752d8f60
Roman T <rt.hagbo@gmail.com>
2022-02-10 18:41:55 +0500

    Дз№2 - «Модули» - ESM (задача со звёздочкой)

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-unused-vars */
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
+
+const game = new Game();
+game.start();
---
+++ src/js/game.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/extensions */
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-unused-vars */
+import Character from './domain.js';
+
+export default class Game {
+  // eslint-disable-next-line class-methods-use-this
+  start() {
+    // eslint-disable-next-line no-console
+    console.log('game started');
+  }
+}
+
+export class GameSavingData {
+
+}
+
+export function readGameSaving() {
+
+}
+
+export function writeGameSaving() {
+
+}
